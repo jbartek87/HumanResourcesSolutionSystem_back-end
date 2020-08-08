ff65827d29bbeeeaad67ab02f7e16263f0ff7a9c
Crycald <krzysiekdybikowski@gmail.com>
2020-08-08 12:18:56 +0200

    basic files

---
+++ src/main/java/com/hrsolutionsystem/hrss/HrssApplication.java
@@ -0,0 +1,13 @@
+package com.hrsolutionsystem.hrss;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HrssApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HrssApplication.class, args);
+    }
+
+}
