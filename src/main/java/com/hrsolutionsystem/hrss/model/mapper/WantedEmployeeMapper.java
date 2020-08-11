d6836b5fc8cb6e2a3a1fa8d30174cea1691012ef
Crycald <krzysiekdybikowski@gmail.com>
2020-08-11 09:27:57 +0200

    added @component annotation to WantedEmployeeMapper

--- src/main/java/com/hrsolutionsystem/hrss/model/mapper/WantedEmployeeMapper.java
+++ src/main/java/com/hrsolutionsystem/hrss/model/mapper/WantedEmployeeMapper.java
@@ -3,7 +3,9 @@ package com.hrsolutionsystem.hrss.model.mapper;
 import com.hrsolutionsystem.hrss.model.domain.WantedEmployee;
 import com.hrsolutionsystem.hrss.model.domain.WantedEmployeeDto;
 import org.mapstruct.Mapper;
+import org.springframework.stereotype.Component;
 
+@Component
 @Mapper
 public interface WantedEmployeeMapper {
     WantedEmployee toMap(WantedEmployeeDto wantedEmployeeDto);
