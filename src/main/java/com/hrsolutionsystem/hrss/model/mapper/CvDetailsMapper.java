8f6b746a15c8ade8e248ba37736b196cb2b5d34b
Crycald <krzysiekdybikowski@gmail.com>
2020-08-13 10:25:24 +0200

    added mapper for cvDetails.class | changed primitives data type fields to wrapper in CvDetailsDto/CvDetails

---
+++ src/main/java/com/hrsolutionsystem/hrss/model/mapper/CvDetailsMapper.java
@@ -0,0 +1,13 @@
+package com.hrsolutionsystem.hrss.model.mapper;
+
+import com.hrsolutionsystem.hrss.model.domain.dto.CvDetailsDto;
+import com.hrsolutionsystem.hrss.model.domain.entity.CvDetails;
+import org.mapstruct.Mapper;
+import org.springframework.stereotype.Component;
+
+@Component
+@Mapper(componentModel = "spring")
+public interface CvDetailsMapper {
+    CvDetails toMap(CvDetailsDto cvDetailsDto);
+    CvDetailsDto toDto(CvDetails cvDetails);
+}
