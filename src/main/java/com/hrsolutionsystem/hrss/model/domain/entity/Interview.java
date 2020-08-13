7df24031c4cdc92c676381f60d6082b473de4a4b
jbartek87 <jbartek87@o2.pl>
2020-08-13 21:40:09 +0200

    created Interview entity with enum class

---
+++ src/main/java/com/hrsolutionsystem/hrss/model/domain/entity/Interview.java
@@ -0,0 +1,30 @@
+package com.hrsolutionsystem.hrss.model.domain.entity;
+
+import com.hrsolutionsystem.hrss.model.domain.enums.InterviewLocation;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Builder
+@Entity
+@Table(name="INTERVIEWS")
+public class Interview {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    @Column(name = "RECRUITERS_ID")
+    private Long recruitersId;
+    @Column(name = "CV_DETAILS_ID")
+    private Long cvDetailsId;
+    @Column(name = "INTERVIEW_DATE")
+    private LocalDate interviewDate;
+    @Column(name = "LOCATION")
+    private InterviewLocation location;
+}
