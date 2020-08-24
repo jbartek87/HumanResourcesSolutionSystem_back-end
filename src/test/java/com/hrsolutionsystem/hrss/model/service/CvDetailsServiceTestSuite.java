9ce1c3cf42b00162a272a96686a6a62853ae52cb
jbartek87 <jbartek87@o2.pl>
2020-08-24 07:58:26 +0200

    started wanted employee test

--- src/test/java/com/hrsolutionsystem/hrss/model/service/CvDetailsServiceTestSuite.java
+++ src/test/java/com/hrsolutionsystem/hrss/model/service/CvDetailsServiceTestSuite.java
@@ -1,64 +1,64 @@
-package com.hrsolutionsystem.hrss.model.service;
-
-import com.hrsolutionsystem.hrss.model.domain.dto.CvDetailsDto;
-import com.hrsolutionsystem.hrss.model.domain.entity.CvFile;
-import com.hrsolutionsystem.hrss.model.domain.enums.CvStatus;
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
-import org.springframework.transaction.annotation.Transactional;
-
-@RunWith(SpringRunner.class)
-@SpringBootTest
-
-public class CvDetailsServiceTestSuite {
-    @Autowired
-    private CvDetailsService service;
-
-    @Test
-    public void saveTest() {
-        CvFile file = new CvFile();
-        CvDetailsDto dto = new CvDetailsDto();
-        dto.setApplyingPosition("Junior Developer Life Matters");
-        dto.setEmail("kokoszka@o2.pl");
-        dto.setFirstName("Kokosza");
-        dto.setLastName("Pojoak");
-        dto.setPhoneNumber(123123123L);
-        dto.setStatus(CvStatus.HIRED);
-        dto.setCoverLetterId(1L);
-        dto.setRecruiterId(1L);
-        dto.setCvFileId(1L);
-
-        CvDetailsDto details = service.save(dto);
-
-
-//        assertNotNull(details);
-    }
-
+//package com.hrsolutionsystem.hrss.model.service;
+//
+//import com.hrsolutionsystem.hrss.model.domain.dto.CvDetailsDto;
+//import com.hrsolutionsystem.hrss.model.domain.entity.CvFile;
+//import com.hrsolutionsystem.hrss.model.domain.enums.CvStatus;
+//import org.junit.Test;
+//import org.junit.runner.RunWith;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.boot.test.context.SpringBootTest;
+//import org.springframework.test.context.junit4.SpringRunner;
+//import org.springframework.transaction.annotation.Transactional;
+//
+//@RunWith(SpringRunner.class)
+//@SpringBootTest
+//
+//public class CvDetailsServiceTestSuite {
+//    @Autowired
+//    private CvDetailsService service;
+//
 //    @Test
-//    public void findById() {
+//    public void saveTest() {
+//        CvFile file = new CvFile();
 //        CvDetailsDto dto = new CvDetailsDto();
 //        dto.setApplyingPosition("Junior Developer Life Matters");
-//        dto.setCoverLetterId(1L);
-//        dto.setCvFileId(1L);
 //        dto.setEmail("kokoszka@o2.pl");
 //        dto.setFirstName("Kokosza");
 //        dto.setLastName("Pojoak");
 //        dto.setPhoneNumber(123123123L);
 //        dto.setStatus(CvStatus.HIRED);
+//        dto.setCoverLetterId(1L);
+//        dto.setRecruiterId(1L);
+//        dto.setCvFileId(1L);
 //
 //        CvDetailsDto details = service.save(dto);
 //
-//        id = details.getId();
 //
-//        assertNotNull(service.findById(id));
+////        assertNotNull(details);
 //    }
 //
-//    @Test(expected = CvDetailsNotFoundException.class)
-//    public void notFound() {
-//        Long notExistingId = 312312553L;
-//        assertNull(service.findById(notExistingId));
-//    }
-}
+////    @Test
+////    public void findById() {
+////        CvDetailsDto dto = new CvDetailsDto();
+////        dto.setApplyingPosition("Junior Developer Life Matters");
+////        dto.setCoverLetterId(1L);
+////        dto.setCvFileId(1L);
+////        dto.setEmail("kokoszka@o2.pl");
+////        dto.setFirstName("Kokosza");
+////        dto.setLastName("Pojoak");
+////        dto.setPhoneNumber(123123123L);
+////        dto.setStatus(CvStatus.HIRED);
+////
+////        CvDetailsDto details = service.save(dto);
+////
+////        id = details.getId();
+////
+////        assertNotNull(service.findById(id));
+////    }
+////
+////    @Test(expected = CvDetailsNotFoundException.class)
+////    public void notFound() {
+////        Long notExistingId = 312312553L;
+////        assertNull(service.findById(notExistingId));
+////    }
+//}
--- src/test/java/com/hrsolutionsystem/hrss/model/service/WantedEmployeeServiceTestSuite.java
+++ src/test/java/com/hrsolutionsystem/hrss/model/service/WantedEmployeeServiceTestSuite.java
@@ -1,7 +1,9 @@
 package com.hrsolutionsystem.hrss.model.service;
 
 import com.hrsolutionsystem.hrss.exception.wantedEmployee.WantedEmployeeNotFoundException;
+import com.hrsolutionsystem.hrss.model.domain.dto.CompanyDto;
 import com.hrsolutionsystem.hrss.model.domain.dto.WantedEmployeeDto;
+import com.hrsolutionsystem.hrss.model.domain.entity.Company;
 import com.hrsolutionsystem.hrss.model.domain.enums.RecruitmentStatus;
 import org.junit.After;
 import org.junit.Before;
@@ -10,6 +12,7 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDate;
 import java.util.ArrayList;
@@ -19,18 +22,19 @@ import static org.junit.Assert.*;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
+@Transactional
 public class WantedEmployeeServiceTestSuite {
     @Autowired
     private WantedEmployeeService service;
 
+    @Autowired
+    private CompanyService companyService;
+
     private Long id;
     private ArrayList<String> skillSet;
     private WantedEmployeeDto dto;
+    private CompanyDto companyDto;
 
-    @After
-    public void dbCleaner() {
-        service.wantedEmployeeDeleteById(id);
-    }
 
     @Before
     public void init() {
@@ -44,21 +48,34 @@ public class WantedEmployeeServiceTestSuite {
         dto.setRecruitmentEnds(LocalDate.now().plusDays(10));
         dto.setRequirements(skillSet);
         dto.setStatus(RecruitmentStatus.ACTIVE);
+        companyDto = new CompanyDto();
+        companyDto.setName("Coders");
+        companyDto.setAddress("Woronicza 17");
+        companyDto.setNip(444L);
+
     }
 
     @Test
     public void wantedEmployeeFindByIdTest() {
+        //Given
+        CompanyDto company = companyService.save(companyDto);
+        dto.setCompanyId(company.getId());
+        //When
         WantedEmployeeDto employee = service.wantedEmployeeSave(dto);
-        id = new Long(employee.getId());
-
+        id = Long.valueOf(employee.getId());
+        //Then
         assertNotNull(service.wantedEmployeeFindById(id));
     }
 
     @Test
     public void wantedEmployeeListTest() {
+        //Given
+        CompanyDto company = companyService.save(companyDto);
+        dto.setCompanyId(company.getId());
+        //When
         WantedEmployeeDto employee = service.wantedEmployeeSave(dto);
-        id = new Long(employee.getId());
-
+        id = employee.getId();
+        //Then
         List<WantedEmployeeDto> list = service.wantedEmployeeGetList();
 
         assertEquals(1, list.size());
@@ -66,9 +83,12 @@ public class WantedEmployeeServiceTestSuite {
 
     @Test
     public void wantedEmployeeUpdate() {
+        //Given
+        CompanyDto company = companyService.save(companyDto);
+        dto.setCompanyId(company.getId());
         WantedEmployeeDto employee = service.wantedEmployeeSave(dto);
         id = new Long(employee.getId());
-
+        //When
         WantedEmployeeDto updateDto = new WantedEmployeeDto();
         updateDto.setId(id);
         updateDto.setRecruitmentStarts(LocalDate.now());
