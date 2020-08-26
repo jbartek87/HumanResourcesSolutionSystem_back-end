cfbe7e7e7e686d7c46cb76a03f92a2304ed02233
jbartek87 <jbartek87@o2.pl>
2020-08-26 13:05:57 +0200

    test fix for interview, cvDetails

--- src/test/java/com/hrsolutionsystem/hrss/model/service/CoverLetterServiceTestSuite.java
+++ src/test/java/com/hrsolutionsystem/hrss/model/service/CoverLetterServiceTestSuite.java
@@ -2,6 +2,7 @@ package com.hrsolutionsystem.hrss.model.service;
 
 import com.hrsolutionsystem.hrss.model.domain.entity.CoverLetter;
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -25,20 +26,17 @@ public class CoverLetterServiceTestSuite {
     private CoverLetterService service;
 
     private MultipartFile multipartFile;
-    private static final String COVER_LETTER_ID = "a45cc045-3185-466e-9d91-a91329060820";
 
+    @Before
     public void init() throws IOException {
-        Path resourceDirectory = Paths.get("src","test","resources", "Noname");
+        Path resourceDirectory = Paths.get("src", "test", "resources", "Noname");
         File file = new File(resourceDirectory.toFile().getAbsolutePath());
         FileInputStream fileInputStream = new FileInputStream(file);
-
         multipartFile = new MockMultipartFile(file.getName(), fileInputStream);
     }
 
     @Test
     public void storeFileTest() throws IOException {
-        init();
-
         try {
             service.storeFile(multipartFile);
         } catch (Exception e) {
@@ -48,7 +46,9 @@ public class CoverLetterServiceTestSuite {
 
     @Test
     public void getFileTest() {
-        CoverLetter coverLetter = service.getFile(COVER_LETTER_ID);
+        service.storeFile(multipartFile);
+        String id = service.getList().get(0).getId();
+        CoverLetter coverLetter = service.getFile(id);
 
         Assert.assertNotNull(coverLetter);
     }
